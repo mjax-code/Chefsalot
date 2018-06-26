acc8d6a1d6758631e8caa06be3eb9274e0232555
mjax-code <mjax.code@gmail.com>
2018-06-25 19:51:50 -0700

    file skeletons and stuff

---
+++ chefsalot/src/Components/Homepage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Topbar from './Topbar';
+class Homepage extends Component {
+  render() {
+    return (
+      <Topbar />
+    );
+  }
+}
+
+export default Homepage;
