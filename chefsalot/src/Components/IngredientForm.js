f20bc21c84e22e08841cfad7e6b2aee0863c6dec
Alex Jackson <ajjackson91195@gmail.com>
2018-07-06 08:27:07 -0700

    Add some basic functionality to recipe form

--- chefsalot/src/Components/IngredientForm.js
+++ chefsalot/src/Components/IngredientForm.js
@@ -3,15 +3,67 @@ import React, { Component } from 'react';
 class IngredientForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {name: ''};
-        
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            ingredient:'',
+            measurement:'',
+            prepDescriptor:'',
+            quantity: '',
+        }
+
+        this.handleIngredientChange = this.handleIngredientChange.bind(this);
+        this.handleIngredientSubmit = this.handleIngredientSubmit.bind(this);
+        this.handleMeasurementChange = this.handleMeasurementChange.bind(this);
+        this.handlePrepDescriptionChange = this.handlePrepDescriptionChange.bind(this);
+        this.handleQuantityChange = this.handleQuantityChange.bind(this);
+      }
+
+      handleIngredientChange(event) {
+          this.setState({ingredient: event.target.value});
+      }
+
+      handleMeasurementChange(event) {
+          this.setState({measurement: event.target.value});
+      }
+
+      handlePrepDescriptionChange(event) {
+          this.setState({prepDescriptor: event.target.value});
+      }
+
+      handleQuantityChange(event) {
+          this.setState({quantity: event.target.value});
+      }
+
+      handleIngredientSubmit() {
+          this.props.onSubmit(this.state.quantity + ' ' +  this.state.measurement
+                    + ' ' + this.state.ingredient);
       }
 
       render() {
-          return 
-
+          return (
+              <div>
+                <form>
+                    <label>
+                        Ingredient:
+                        <input onChange={this.handleIngredientChange} type="text" />
+                    </label>
+                    <label>
+                        Measurement:
+                        <input onChange={this.handleMeasurementChange} type="text" />
+                    </label>
+                    <label>
+                        Preperation Description:
+                        <input onChange={this.handlePrepDescriptionChange} type="text" />
+                    </label>
+                    <label>
+                        Quantity:
+                        <input onChange={this.handleQuantityChange} type="text" />
+                    </label>
+                </form>
+                <button onClick={this.handleIngredientSubmit}>
+                    Add Ingredient 
+                </button>
+              </div>
+          );
       }
 }
 
--- chefsalot/src/Components/RecipeForm.js
+++ chefsalot/src/Components/RecipeForm.js
@@ -5,27 +5,83 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { Input } from '@material-ui/core';
 import IngredientAddForm from './IngredientAddForm';
+import IngredientForm from './IngredientForm';
 
 class RecipeForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        ingredients: ["hi", "matt"],
+        ingredients: [],
+        submittedIngredients: [],
+
+        servings: '',
+        directions: '',
+        cookTime: '',
+        recipe: '',
     };
+
+    this.handleIngredientSubmit =  this.handleIngredientSubmit.bind(this);
+    this.handleIngredientAddSubmit =  this.handleIngredientAddSubmit.bind(this);
+    this.handleServingsChange = this.handleServingsChange.bind(this);
+    this.handleCookTimeChange = this.handleCookTimeChange.bind(this);
+    this.handleDirectionsChange = this.handleDirectionsChange.bind(this);
+    this.handleRecipeFormSubmit = this.handleRecipeFormSubmit.bind(this);
+  }
+
+  handleIngredientAddSubmit() {
+      this.setState(
+        {ingredients: this.state.ingredients.concat([
+            <IngredientForm onSubmit={this.handleIngredientSubmit}/>
+        ])})
+  }
+
+  handleIngredientSubmit(ingredientSubmit) {
+      this.setState({submittedIngredients: this.state.submittedIngredients.concat([ingredientSubmit])});
+  }
+
+  handleServingsChange(event) {
+      this.setState({servings: event.target.value});
+  }
+
+  handleCookTimeChange(event) {
+      this.setState({cookTime: event.target.value});
+  }
+
+  handleDirectionsChange(event) {
+      this.setState({directions: event.target.value});
+  }
+
+  handleRecipeFormSubmit() {
+      let recipe = this.state.servings + ' ' + this.state.directions + ' ' + this.state.cookTime;
+      this.setState({recipe: recipe});
   }
 
   render() {
-    var my_ingredients = this.state.ingredients;
-    
     return(
         <div>
             <IngredientAddForm 
-                onSubmit={() => 
-                    this.setState(
-                        {ingredients: this.state.ingredients.concat(['hello alex'])}
-                    )
-            }/>
+                onSubmit={this.handleIngredientAddSubmit} />
             <IngredientList ingredients={this.state.ingredients} />
+            <IngredientList ingredients={this.state.submittedIngredients} />
+            <form id="recipeForm">
+                <label>
+                    Servings:
+                    <input onChange={this.handleServingsChange} type="text" />
+                </label>
+                <label>
+                    Cook Time:
+                    <input onChange={this.handleCookTimeChange} type="text" />
+                </label>
+            </form>
+            <label>
+                Directions: 
+                <textarea onChange={this.handleDirectionsChange} form="recipeForm">
+                </textarea>
+            </label>
+            <button onClick={this.handleRecipeFormSubmit}>
+                Add Recipe!
+            </button>
+            {this.state.recipe}
         </div>
     );
   }
