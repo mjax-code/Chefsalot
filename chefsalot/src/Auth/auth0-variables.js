e21ddc0401761230e6caae6bd6d101fa58d0d5a6
Alex Jackson <ajjackson91195@gmail.com>
2018-07-08 09:52:54 -0700

    Revert "fix merge conflicts"
    This reverts commit c7ed558ee8257559576906c84ce414e99738092b, reversing
    changes made to 2df0e24b3a17931547c8dcf689234e8ce92d2b4e.

--- chefsalot/src/Auth/auth0-variables.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CONFIG = {
-  domain: 'mjax.auth0.com',
-  clientId: 'zMGuvScYDaSb5gHETfMm1VbSg21CEAhX',
-  callbackUrl: 'http://localhost:3000/callback'
-};
--- chefsalot/src/index.js
+++ chefsalot/src/index.js
@@ -1,11 +1,8 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import { makeMainRoutes } from './routes';
+import Homepage from './Components/Homepage';
+import registerServiceWorker from './registerServiceWorker';
 
-const routes = makeMainRoutes();
-
-ReactDOM.render(
-  routes,
-  document.getElementById('root')
-);
+ReactDOM.render(<Homepage />, document.getElementById('root'));
+registerServiceWorker();
