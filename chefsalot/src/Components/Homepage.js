176ffdca06ea230544b80d370cbbae0f90fd6e30
Alex Jackson <ajjackson91195@gmail.com>
2018-08-14 08:18:16 -0700

    Replace NavBar with routes

--- chefsalot/src/Components/Homepage.js
+++ chefsalot/src/Components/Homepage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import Topbar from 'Components/Topbar';
 import RecipeForm from 'Components/RecipeForm';
 import GoogleLogoutForm from 'Components/Authentication/GoogleLogoutForm';
 import RecipeList from 'Components/UserViewComponents/RecipeList';
 import UserView from 'Components/UserViewComponents/UserView';
 import GroupView from 'Components/GroupViewComponents/GroupView';
 import { connect } from 'react-redux';
+import { Link, Route, Switch } from 'react-router-dom';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Button from '@material-ui/core/Button';
 
 const mapStateToProps = state => {
   return {
@@ -17,12 +20,9 @@ class Homepage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      body_component: 'userview',
       recipe_list: null
     };
 
-    this.getBodyComponent = this.getBodyComponent.bind(this);
-    this.handleNav = this.handleNav.bind(this);
     this.handleRecipeLoad = this.handleRecipeLoad.bind(this);
   }
 
@@ -30,34 +30,35 @@ class Homepage extends Component {
     this.setState({ recipe_list: recipe_list });
   }
 
-  handleNav(body_component) {
-    this.setState({ body_component: body_component });
-  }
-
-
-  getBodyComponent() {
-    switch (this.state.body_component) {
-      case "userview":
-        return <UserView recipe_list_view={<RecipeList onRecipeLoad={this.handleRecipeLoad}
-          recipe_list={this.state.recipe_list}
-          token={this.props.auth_token} />} />
-      case "groupview":
-        return <GroupView token={this.props.auth_token} />
-      case "recipeview":
-        return <RecipeForm token={this.props.auth_token} />
-      default:
-        return <UserView />
-    }
-  }
-
   render() {
     return (
         <div className="chefsalot-home-page">
           <div className="home-page-container">
-            {/* TODO have a better way of handling these different body components ... enum? */}
             <GoogleLogoutForm />
-            <Topbar onNav={this.handleNav} userview="userview" groupview="groupview" recipeview="recipeview" />
-            {this.getBodyComponent()}
+            <List>
+              <ListItem>
+               <Button><Link to="/recipes">User</Link></Button> 
+              </ListItem>
+              <ListItem>
+               <Button><Link to="/groups">Groups</Link></Button> 
+              </ListItem>
+              <ListItem>
+               <Button><Link to="/add_recipe">Add Recipe</Link></Button> 
+              </ListItem>
+            </List>
+            <Switch>
+              <Route path="/recipes"
+                     render={(props) => <UserView {...props} recipe_list_view={<RecipeList onRecipeLoad={this.handleRecipeLoad}
+                                          recipe_list={this.state.recipe_list}
+                                          token={this.props.auth_token} />} />}
+                      />
+              <Route path="/groups" 
+                     render= {(props) => <GroupView {...props} token={this.props.auth_token} /> } 
+                      />
+              <Route path="/add_recipe" 
+                     render= {(props) => <RecipeForm {...props} token={this.props.auth_token} /> } 
+                      />
+            </Switch>
           </div>
         </div>
       );
